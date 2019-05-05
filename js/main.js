cf51f1e47778efd2722f5ca746c011c2019bacd5
Kwinnieprince <kwinten@delrue.com>
2019-05-05 14:11:30 +0200

    updates

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ window.onload = () => {
   });
 };
 
+
 function home() {
   let welcome_html =
   "<h1>Welkom op het project van de Mechelsesteenweg</h1>" +
@@ -16,6 +17,9 @@ function home() {
 }
 
 function luftdaten() {
+  $.ajaxSetup({
+    async: false
+  });
   $(document).ready(function () {
     // let values = getFineParicles();
     // let humidityValues = getTemperatureAndHumidity();
@@ -37,7 +41,7 @@ function luftdaten() {
       "</div>" +
       "</div>" +
       "</div>";
-    $.getJSON('https://api.luftdaten.info/v1/sensor/24242/').done(function (data){
+    $.getJSON('https://api.luftdaten.info/v1/sensor/24242/').done(function (data) {
       $.each(data, function (i, field) {
         humidity = field.sensordatavalues[0].value;
         temperature = field.sensordatavalues[1].value;
@@ -45,9 +49,9 @@ function luftdaten() {
         console.log(field.sensordatavalues[1].value); //Temperature
       })
     });
-    $.getJSON('https://api.luftdaten.info/v1/sensor/24241/').done(function (data){
+    $.getJSON('https://api.luftdaten.info/v1/sensor/24241/').done(function (data) {
       $.each(data, function (i, field) {
-        if (field.sensordatavalues[0]){
+        if (field.sensordatavalues[0]) {
           fineP1 = field.sensordatavalues[0].value;
           fineP2 = field.sensordatavalues[1].value;
           console.log(fineP1 + "P1"); //P1
@@ -55,94 +59,84 @@ function luftdaten() {
         }
       })
     });
-    setTimeout( function(){
     console.log("P1 " + fineP1 + "  P2 " + fineP2);
-
-    if (fineP1 < 25.00){
-      fineP1div = '<div id="p1div" class="alert alert-succes">Er is ' + fineP1 + ' mg/m<sup>3</sup> aan partikels van 2.5 &#181;m</div>'
-    }else if (fineP1 >= 25.00 && fineP1 < 50.00 ){
-      fineP1div = '<div id="p1div" class="alert alert-danger">Er is ' + fineP1 + ' mg/m<sup>3</sup> aan partikels van 2.5 &#181;m</div>'
+    if (fineP1 < 25.00) {
+      fineP1div = '<div id="p1div" class="alert alert-success">Er zijn ' + fineP1 + ' mg/m<sup>3</sup> aan partikels van 2.5 &#181;m</div>'
+    } else if (fineP1 >= 25.00 && fineP1 < 50.00) {
+      fineP1div = '<div id="p1div" class="alert alert-warning">Er zijn ' + fineP1 + ' mg/m<sup>3</sup> aan partikels van 2.5 &#181;m</div>'
     } else {
-      fineP1div = '<div id="p1div" class="alert alert-danger">Er is ' + fineP1 + ' mg/m<sup>3</sup> aan partikels van 2.5 &#181;m</div>'
+      fineP1div = '<div id="p1div" class="alert alert-danger">Er zijn ' + fineP1 + ' mg/m<sup>3</sup> aan partikels van 2.5 &#181;m</div>'
     }
 
-    if (fineP2 < 25){
-      fineP2div = '<div id="p2div" class="alert alert-succes">Er is ' + fineP2 + ' mg/m<sup>3</sup> aan partikels van 10 &#181;m</div>'
-    }else if (fineP2 >= 25 && fineP2 < 50 ){
-      fineP2div = '<div id="p2div" class="alert alert-danger">Er is ' + fineP2 + ' mg/m<sup>3</sup> aan partikels van 10 &#181;m</div>'
+    if (fineP2 < 25) {
+      fineP2div = '<div id="p2div" class="alert alert-success">Er zijn ' + fineP2 + ' mg/m<sup>3</sup> aan partikels van 10 &#181;m</div>'
+    } else if (fineP2 >= 25 && fineP2 < 50) {
+      fineP2div = '<div id="p2div" class="alert alert-warning">Er zijn ' + fineP2 + ' mg/m<sup>3</sup> aan partikels van 10 &#181;m</div>'
     } else {
-      fineP2div = '<div id="p2div" class="alert alert-danger">Er is ' + fineP2 + ' mg/m<sup>3</sup> aan partikels van 10 &#181;m</div>'
+      fineP2div = '<div id="p2div" class="alert alert-danger">Er zijn ' + fineP2 + ' mg/m<sup>3</sup> aan partikels van 10 &#181;m</div>'
     }
 
+    if (humidity < 35) {
+      humiditydiv = '<div id="humiditydiv" class="alert alert-danger">De luchtvochtigheid is ' + humidity + ' %</div>'
+    } else if (humidity >= 35 && humidity < 70) {
+      humiditydiv = '<div id="humiditydiv" class="alert alert-success">De luchtvochtigheid is ' + humidity + ' %</div>'
+    } else {
+      humiditydiv = '<div id="humiditydiv" class="alert alert-warning">De luchtvochtigheid is ' + humidity + ' %</div>'
+    }
+
+    if (temperature < 0) {
+      temperaturediv = '<div id="temperaturediv" class="alert alert-primary">De temperatuur is ' + temperature + ' <supq>º</supq>C</div>'
+    } else if (temperature >= 15 && temperature < 30) {
+      temperaturediv = '<div id="temperaturediv" class="alert alert-success">De temperatuur is ' + temperature + ' <supq>º</supq>C</div>'
+    } else {
+      temperaturediv = '<div id="temperaturediv" class="alert alert-danger">De temperatuur is ' + temperature + ' <supq>º</supq>C</div>'
+    }
     $('#main').empty();
     $('#main').html(iframeHtml);
     $('#calculatedvalues').append(fineP1div);
     $('#calculatedvalues').append(fineP2div);
-  }, 1000);
-  })
-}
-
-// function getFineParicles() {
-//   $.getJSON('https://api.luftdaten.info/v1/sensor/24241/').done(function (data){
-//     $.each(data, function (i, field) {
-//       if (field.sensordatavalues[0]){
-//         let values = [2];
-//         return field.sensordatavalues[0].value; // P1
-//         // values[1] = field.sensordatavalues[1].value; //P2
-//         // return values;
-//       }
-//     })
-//   });
-// }
-
-// function getTemperatureAndHumidity() {
-//   $.getJSON('https://api.luftdaten.info/v1/sensor/24242/').done(function (data){
-//     $.each(data, function (i, field) {
-//       let valuesTemp = [2];
-//       values[0] = field.sensordatavalues[0].value; // humidity
-//       values[1] = field.sensordatavalues[1].value; // temperature
-//       return valuesTemp;
-//     })
-//   });
-// }
-
-function telraam() {
-  let html =
-    "<div class='container-fluid full-height no-padding'>" +
-      "<div class='row full-height no-padding'>" +
-        "<div class='col-sm full-height no-padding'>" +
-          "<div id='iframeOuter'>" +
-            "<iframe src='https://telraam.net' class='full-height full-width' id='iframe'></iframe>" +
-          "</div>" +
-        "</div>" +
-      "</div>" +
-    "</div>";
-  $('#main').empty();
-  $('#main').html(html);
-  // removeFooter();
-}
-
-function removeFooter(){
-  $(document).ready(function(){
-    $('#iframe').contents().find('footer.site-footer.no-top-margin').remove();
-    $('#iframe').contents().find('section.navigation').remove();
-
-    // $("footer.site-footer.no-top-margin ").empty();
-    // $("section.navigation").empty();
+    $('#calculatedvalues').append(humiditydiv);
+    $('#calculatedvalues').append(temperaturediv);
   });
-}
 
-function information() {
-  let information_html =
-    "<div class='container-fluid full-height no-padding'>" +
-    "<div class='row full-height no-padding'>" +
-    "<div class='col-sm full-height no-padding'>" +
-    "<div id='iframeOuter'>" +
-    "<iframe src='https://telraam.net/nl/location/348180/2019-04-21/2019-05-05' class='full-height full-width' id='iframe'></iframe>" +
-    "</div>" +
-    "</div>" +
-    "</div>" +
-    "</div>";
-  $('#main').empty();
-  $('#main').html(information_html);
+  function telraam() {
+    let html =
+      "<div class='container-fluid full-height no-padding'>" +
+      "<div class='row full-height no-padding'>" +
+      "<div class='col-sm full-height no-padding'>" +
+      "<div id='iframeOuter'>" +
+      "<iframe src='https://telraam.net' class='full-height full-width' id='iframe'></iframe>" +
+      "</div>" +
+      "</div>" +
+      "</div>" +
+      "</div>";
+    $('#main').empty();
+    $('#main').html(html);
+    // removeFooter();
+  }
+
+  function removeFooter() {
+    $(document).ready(function () {
+      $('#iframe').contents().find('footer.site-footer.no-top-margin').remove();
+      $('#iframe').contents().find('section.navigation').remove();
+
+      // $("footer.site-footer.no-top-margin ").empty();
+      // $("section.navigation").empty();
+    });
+  }
+
+  function information() {
+    let information_html =
+      "<div class='container-fluid full-height no-padding'>" +
+      "<div class='row full-height no-padding'>" +
+      "<div class='col-sm full-height no-padding'>" +
+      "<div id='iframeOuter'>" +
+      "<iframe src='https://telraam.net/nl/location/348180/2019-04-21/2019-05-05' class='full-height full-width' id='iframe'></iframe>" +
+      "</div>" +
+      "</div>" +
+      "</div>" +
+      "</div>";
+    $('#main').empty();
+    $('#main').html(information_html);
+  }
 }
